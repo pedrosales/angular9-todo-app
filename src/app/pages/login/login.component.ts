c0fa44093be7080f87704b390fe477694662fc01
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-15 19:02:41 -0300

    login com google

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth
+  ) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.afAuth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
 }
